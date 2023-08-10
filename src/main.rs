e193cfc76e33706e2e0497cfb8e5441b1abbf2f9
Sam Zanca <samuele@zanca.dev>
2023-08-10 02:16:01 -0400

    bevy hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+   App::new()
+    .add_plugins(
+      DefaultPlugins
+        .set(ImagePlugin::default_nearest()) // tbh, already forgot what this does
+        .set(WindowPlugin {
+          primary_window: Some(Window {
+            title: "Risk of Bevy".into(),
+            resolution: (640.0, 480.0).into(),
+            resizable: false,
+            ..default()
+          }),
+          ..default()
+        })
+    )
+    .run()
 }
